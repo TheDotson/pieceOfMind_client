8afffa4c8cf3bd8c7c9899f40c1c722600d4ed86
Michael Dotson <thedotson@icloud.com>
2021-03-20 12:24:14 -0500

    added navbar

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,13 +4,6 @@ export const Home = props => {
   return (
     <>
       <h1>Home</h1>
-      <button className="log-link fakeLink"
-        onClick={() => {
-            localStorage.removeItem("token")
-            localStorage.removeItem("user_id")
-            props.history.push({ pathname: "/" })
-        }}
-    >Logout Test</button>
     </>
   );
 };
